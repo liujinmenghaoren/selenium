30a0a12b55593275f7bb510062b810a049553587
liujinmeng <1325479537@qq.com>
2016-12-06 15:48:21 +0800

    add 14case

---
+++ 14case/loginlocator.js
@@ -0,0 +1,9 @@
+var loginlocator = {
+	username: "id=username",
+	password: "id=password",
+	btnlogin: "name=login",
+	errmsg: "id=flash_error",
+	loginname: "id=loggedas",
+	logout: "link=退出",
+	outname: "css=h1"
+}
